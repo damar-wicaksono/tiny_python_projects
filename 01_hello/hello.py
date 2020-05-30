43fa93b96fe8c1fa7a49f3f3b15bf2c7b319479e
Damar Wicaksono <damar.wicaksono@outlook.com>
2020-05-30 11:51:07 +0200

    Update style according to yapf

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
-# Purpose: Say hello
+"""
+Purpose: Say hello
+"""
 
 import argparse
 
+
+# ----------------------------------------------------------------------------
 def get_args():
+    """Get command line arguments"""
     parser = argparse.ArgumentParser(description="Say hello")
-    parser.add_argument("-n", "--name", metavar="name", default="World", help="Name to greet")
+    parser.add_argument("-n",
+                        "--name",
+                        metavar="name",
+                        default="World",
+                        help="Name to greet")
     return parser.parse_args()
 
+
+# ----------------------------------------------------------------------------
 def main():
+    """Say hello to someone"""
     args = get_args()
 
     print("Hello, {}!".format(args.name))
 
+
+# ----------------------------------------------------------------------------
 if __name__ == "__main__":
     main()
-
