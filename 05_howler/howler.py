b6c396c7398aab1effba3f0b07c879c50f8234f1
Damar Wicaksono <damar.wicaksono@outlook.com>
2020-06-05 20:06:12 +0200

    Remove unnecessary command line arguments

--- 05_howler/howler.py
+++ 05_howler/howler.py
@@ -13,38 +13,19 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Howler (upper-cases input)',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
+    parser.add_argument('str',
                         metavar='str',
-                        help='A positional argument')
+                        help='Input string or a file')
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
+    parser.add_argument('-o',
+                        '--outfile',
+                        help='Output filename',
                         metavar='str',
                         type=str,
                         default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('r'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
 
     return parser.parse_args()
 
@@ -54,17 +35,11 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    str_arg = args.str
+    outfile_arg = args.outfile
 
     print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    print(f'outfile_arg = "{outfile_arg}"')
 
 
 # --------------------------------------------------
